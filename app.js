92292249818de46d43f7583618ce8f87158cc929
khoa6430 <khoa6430@gmail.com>
2022-01-18 16:23:18 +0700

    commit 1

---
+++ app.js
@@ -0,0 +1,62 @@
+
+//DOM - DOCUMENT OBJECT 
+// console.log(document);
+
+// //HTML Selector
+// //ID
+// const app = document.getElementById("app");
+// console.log(app);
+
+// //Class Name
+// const apps = document.getElementsByClassName("app-1");
+// console.log(apps);
+// console.log(apps[0]);
+// console.log(apps[1]);
+
+// //Tag name
+// const divList = document.getElementsByTagName("div");
+// console.log(divList);
+
+// //QuerySelector, QuerySelectorAll(id="#id,className = "className", tagname="div");
+
+// //Important Properties Methods
+// //Properties
+// console.dir(app);
+
+// //text content
+// console.log(app.innerHTML);
+// app.innerHTML="<i>HELLO KHOA ĐÃ IN NGHIÊNG</i>"
+// console.log(app.innerText);
+// app.innerText = "<i>Hello everyone</i>";
+
+// //style
+// console.log(app.style);
+// console.log(app.style.backgroundColor);
+// console.log(app.style.backgroundColor="red");
+
+
+//CHIRLDRESS, PARENTELEMENT
+// const ul = document.getElementById("list");
+// console.log(ul);
+// console.log(ul.children[1]);
+// //Bo bullet
+// console.log(ul.style.listStyle="none");
+// console.log(ul.id);
+
+// console.log(ul.classList);
+// ul.classList.add("cl4");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
